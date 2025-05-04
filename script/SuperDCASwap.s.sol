812c965e6af5a336d6150d97e5332aafeecfd9c6
Michael Ghen <mike@mikeghen.com>
2025-05-04 17:25:45 -0400

    Add deploy script

---
+++ script/SuperDCASwap.s.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
+import {Script} from "forge-std/Script.sol";
+import {SuperDCASwap} from "../src/SuperDCASwap.sol";
+import {console} from "forge-std/console.sol";
+
+contract DeploySuperDCASwap is Script {
+    address constant UNIVERSAL_ROUTER_ADDRESS = 0xf70536B3bcC1bD1a972dc186A2cf84cC6da6Be5D;
+    address constant POOL_MANAGER_ADDRESS = 0x00B036B58a818B1BC34d502D3fE730Db729e62AC;
+    address constant PERMIT2_ADDRESS = 0x000000000022D473030F116dDEE9F6B43aC78BA3;
+
+    function run() external returns (SuperDCASwap) {
+        console.log("Deploying SuperDCASwap with:");
+        console.log("  Universal Router:", UNIVERSAL_ROUTER_ADDRESS);
+        console.log("  Pool Manager:", POOL_MANAGER_ADDRESS);
+        console.log("  Permit2:", PERMIT2_ADDRESS);
+
+        vm.startBroadcast();
+
+        SuperDCASwap swapContract = new SuperDCASwap(UNIVERSAL_ROUTER_ADDRESS, POOL_MANAGER_ADDRESS, PERMIT2_ADDRESS);
+
+        vm.stopBroadcast();
+
+        console.log("SuperDCASwap deployed at:", address(swapContract));
+        return swapContract;
+    }
+}
